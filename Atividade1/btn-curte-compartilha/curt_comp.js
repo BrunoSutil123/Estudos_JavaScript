ae8f8ec277b398527700671de0605aaa333981dc
Bruno de Oliveira Sutil <brunosutil30@gmail.com>
2026-03-13 08:28:16 -0300

    feat: adiciona exercício 3 com botões de curtir e compartilhar.

---
+++ Atividade1/btn-curte-compartilha/curt_comp.js
@@ -0,0 +1,11 @@
+let botaoLike = document.querySelector(".btnLike");
+
+botaoLike.addEventListener ("click", function(){
+    alert("Deixou o Like!!");
+});
+
+let botaoShare = document.querySelector(".btnCompartilha");
+
+botaoShare.addEventListener ("click", function(){
+    alert("Compartilhou!!");
+});
