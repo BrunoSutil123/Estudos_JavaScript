9b92f6d5c4f189b9d2fecca5f1ae2013d694b2e3
Bruno de Oliveira Sutil <brunosutil30@gmail.com>
2026-03-14 13:57:13 -0300

    feat: Desenvolve verificador de maior idade com if e else.

---
+++ Atividade3/Verificar-idade/script.js
@@ -0,0 +1,25 @@
+/*
+
+Exercício 1
+
+Crie:
+ - variável idade
+ - Se idade ≥ 18
+ - Mostre "Maior de idade"
+
+Senão:
+ - "Menor de idade"
+
+*/
+
+let idade = 16;
+
+if (idade >= 1) {
+    if (idade >= 18){
+        console.log("Maior de idade");
+    }else{
+        console.log("Menor de idade");
+    }
+}else{
+    console.log("Valor inválido!");
+}
